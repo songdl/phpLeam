1e2f97211738ea9ca4e462dfd3b30288111a4048
songdl <songdl@songdl-pc>
2015-07-21 23:11:46 +0800

    phpinfo()测试1。

---
+++ forum.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * songdl 2015-07-21 23:08
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+phpinfo();
--- index.php
+++ index.php
@@ -6,4 +6,15 @@
  * and open the template in the editor.
  */
 
-phpinfo();
+$url = '';
+$domain = $_ENV = array();
+$_ENV['domain'] = $domain;
+if (empty($_ENV['domain'])){
+    $_ENV['curapp'] = 'forum';
+}
+
+if (!empty($url)){
+    
+} else {
+    require './' .$_ENV['curapp'] . '.php';
+}
